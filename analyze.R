07dea0b3bd18cf467e621dab7e8b4386306808bb
Jim V <vlandham@gmail.com>
2014-04-22 21:05:23 -0700

    groupings

--- analyze.R
+++ analyze.R
@@ -5,15 +5,20 @@ require('ddply')
 require('ggthemes')
 
 data <- read.delim("api.tsv", sep="\t", header=TRUE)
+docs <- read.delim("data/docs.tsv", sep="\t", header=TRUE)
 colnames(data) <- c("origin", "target", "count", "total")
 data$frequency <- data$count / data$total
 
+data <- merge(data, docs, by.x = "origin", by.y="command", sort = FALSE, all.x = TRUE, all.y = FALSE)
+no_main <- (subset(data, is.na(main)))
+
 nodes <- unique(data['origin'])
 
-node_connections <- ddply(data, "origin", summarize, links = length(target))
+node_connections <- ddply(data, "origin", summarize, links = length(target), type=main[1])
 node_connections <- node_connections[with(node_connections, order(-links)), ]
+#write.table(data.frame(node_connections[is.na(node_connections$type),"origin"], 'Geo', 'Plugin'), "missing.csv", row.names=FALSE, quote=FALSE, sep="\t")
 
-node_totals <- ddply(data, "origin", summarize, total = total[1])
+node_totals <- ddply(data, "origin", summarize, total = total[1], type=main[1])
 node_totals <- node_totals[with(node_totals, order(-total)), ]
 
 
@@ -22,7 +27,7 @@ m <- m + geom_histogram(binwidth=35) + theme_economist()
 ggsave("out/histogram_counts.pdf", m)
 
 
-p <- ggplot(node_totals, aes(x=factor(origin, levels=as.character(node_totals$origin)), y=total)) + labs(title="Related Blocks per API Call", y="Number of Related Blocks", x= "")
+p <- ggplot(node_totals, aes(x=factor(origin, levels=as.character(node_totals$origin)), y=total, fill=type)) + labs(title="Related Blocks per API Call", y="Number of Related Blocks", x= "")
 p <- p + geom_bar(stat = "identity") + coord_flip() + theme_economist()
 ggsave("out/all_sizes.pdf", p, width = 12, height = 42)
 
@@ -47,7 +52,7 @@ high_counts <- subset(data,count >= 50)
 m <- ggplot(low_counts, aes(x=count)) + labs(title="Coocurrance Raw Values - Only Low")
 m + geom_histogram(binwidth=1) + theme_economist()
 
-p <- ggplot(node_connections, aes(x=factor(origin, levels=as.character(node_connections$origin)), y=links)) + labs(title="Coocurances per API call")
+p <- ggplot(node_connections, aes(x=factor(origin, levels=as.character(node_connections$origin)), y=links, fill=type)) + labs(title="Coocurances per API call")
 p <- p + geom_bar(stat = "identity") + coord_flip() + theme_economist()
 ggsave("out/connection_counts.pdf", p, width = 12, height = 42)
 
--- tools/parse_docs.rb
+++ tools/parse_docs.rb
@@ -13,9 +13,23 @@ file = File.open(output_filename, 'w')
 file.puts ["command", "main", "sub"].join("\t")
 lines.each do |line|
   if line =~ /^###.*\[(.*)\]/
-    current_sub = $1
+    current_sub = $1.gsub("]","").split("|")[0].split("(")[0].strip
+    if line =~ /\]\((.*)\)$/
+      current_sub = $1
+    end
   elsif line =~ /^##.*\[(.*)\]/
     current_main = $1
+    command = nil
+    if current_main =~ /^d3/
+      command = current_main.split("(")[0].strip
+    end
+    current_main = current_main.gsub("]","").split("|")[0].split("(")[0].strip
+    if line =~ /\]\((.*)\)$/
+      current_main = $1
+    end
+    if command
+      file.puts [command, current_main, 'base'].join("\t")
+    end
   elsif line =~ /^\*.*\[(.*)\]/
     command = $1.split("|")[0]
     file.puts [command, current_main, current_sub].join("\t")
